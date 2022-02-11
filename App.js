c60144ede5623b33befb15668918865b96598e04
irumiantcev <rumis@live.ru>
2022-02-11 16:24:59 +0300

    local Database

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
@@ -6,12 +6,22 @@ import 'react-native-gesture-handler';
 
 import store from './src/store';
 import { AppNavigation } from './src/navigation/AppNavigation';
+import { DB } from './src/db';
 
 export default function App() {
-    let [fontsLoaded] = useFonts({
+    const [fontsLoaded] = useFonts({
         'open-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
         'open-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
     });
+
+    useEffect(async () => {
+        try {
+            await DB.init();
+            console.log('Database ready....');
+        } catch (e) {
+            console.log('Error:', e);
+        }
+    }, []);
 
     if (!fontsLoaded) {
         return <AppLoading />;
